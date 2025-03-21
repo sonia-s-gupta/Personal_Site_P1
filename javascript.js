650eff8f7b544c96ab0dfbe1b7310373dcc97901
Sonia Gupta <soniagupta@Sonias-Air.attlocal.net>
2025-03-21 00:12:36 -0400

    Updated pages and added new contact form and success pages

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+//Get form and button elements by ID
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+//Add event listener to form
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+
+    //Prevent form's default behavior
+  e.preventDefault()
+
+  //Redirect to success page after submission
+  window.location.href = window.location.origin + '/success.html'
+})
